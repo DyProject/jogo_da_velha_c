40df8f7d6f6b6b5bc02331e76fe9c2d67281c0d4
Diego Rodrigues <diegomrodz@users.noreply.github.com>
2016-09-05 20:45:45 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define BOARD_SIZE 3
-#define VICTORY_COUNT 3
+#define BOARD_SIZE 5
 
 typedef enum {
-	X, // Jogador 1 
+	X, // Jogador 1
 	O, // Jogador 2 ou IA
 	E  // Vazio (Empty)
 } Player;
 
-typedef struct 
+typedef struct
 {
 	Player places[BOARD_SIZE][BOARD_SIZE];
 	unsigned int round;
@@ -19,13 +18,13 @@ typedef struct
 	unsigned int is_win_scenario;
 } Board;
 
-typedef struct 
+typedef struct
 {
 	int row;
 	int col;
 } Move;
 
-Board new_board() 
+Board new_board()
 {
 	Board b;
 	int i, j;
@@ -33,9 +32,9 @@ Board new_board()
 	b.round = 1;
 	b.weight = 0;
 
-	for (i = 0; i < BOARD_SIZE; i += 1) 
+	for (i = 0; i < BOARD_SIZE; i += 1)
 	{
-		for (j = 0; j < BOARD_SIZE; j += 1) 
+		for (j = 0; j < BOARD_SIZE; j += 1)
 		{
 			b.places[i][j] = E;
 		}
@@ -44,7 +43,7 @@ Board new_board()
 	return b;
 }
 
-Board new_board_from(Board b) 
+Board new_board_from(Board b)
 {
 	Board new;
 	int i, j;
@@ -52,9 +51,9 @@ Board new_board_from(Board b)
 	new.round = b.round;
 	new.weight = b.weight;
 
-	for (i = 0; i < BOARD_SIZE; i += 1) 
+	for (i = 0; i < BOARD_SIZE; i += 1)
 	{
-		for (j = 0; j < BOARD_SIZE; j += 1) 
+		for (j = 0; j < BOARD_SIZE; j += 1)
 		{
 			new.places[i][j] = b.places[i][j];
 		}
@@ -63,14 +62,14 @@ Board new_board_from(Board b)
 	return new;
 }
 
-char player_to_str(Player p) 
+char player_to_str(Player p)
 {
 	if (p == X) return 'X';
 	if (p == O) return 'O';
 	if (p == E) return ' ';
 }
 
-void print_board(Board b) 
+void print_board(Board b)
 {
 	int i, j, k;
 
@@ -79,34 +78,34 @@ void print_board(Board b)
 	printf("Round: %d\n", b.round);
 	printf("Peso: %d\n\n", b.weight);
 
-	for (i = 0; i < BOARD_SIZE; i += 1) 
+	for (i = 0; i < BOARD_SIZE; i += 1)
 	{
 		printf(" %d  ", i);
 	}
 
 	printf("\n");
 
-	for (i = 0; i < BOARD_SIZE; i += 1) 
+	for (i = 0; i < BOARD_SIZE; i += 1)
 	{
-		for (j = 0; j < BOARD_SIZE; j += 1) 
+		for (j = 0; j < BOARD_SIZE; j += 1)
 		{
 			printf(" %c ", player_to_str(b.places[i][j]));
 
-			if (j != BOARD_SIZE - 1) 
+			if (j != BOARD_SIZE - 1)
 			{
 				printf("|");
 			}
-			else 
+			else
 			{
 				printf(" %d", i);
 			}
 		}
 
-		if (i != BOARD_SIZE - 1) 
+		if (i != BOARD_SIZE - 1)
 		{
 			printf("\n");
 
-			for (k = 0; k < BOARD_SIZE * 4; k += 1) 
+			for (k = 0; k < BOARD_SIZE * 4; k += 1)
 			{
 				printf("-");
 			}
@@ -118,14 +117,14 @@ void print_board(Board b)
 	printf("\n");
 }
 
-int comp_char(char that, char with) 
+int comp_char(char that, char with)
 {
 	return (int)that == (int)with;
 }
 
-int check_move(Board* b, int row, int col) 
+int check_move(Board* b, int row, int col)
 {
-	if (row >= BOARD_SIZE || col >= BOARD_SIZE) 
+	if (row >= BOARD_SIZE || col >= BOARD_SIZE)
 	{
 		return 0;
 	}
@@ -136,92 +135,47 @@ int check_move(Board* b, int row, int col)
 /**
  * Verifica se há um vencedor no jogo e o retorna.
  */
-Player has_winner(Board b) 
+Player has_winner(Board b)
 {
-	int i, j, k, m, n;
-	int has_won_col, has_won_row, has_won_d, has_won_ad;
-	Player lc_p, mid_p;
+	int i, j, k;
+	int has_won_col, has_won_row;
+	Player row_p, col_p, mid_p;
 
-	// Espaço disponível para buscar do meio do jogo
-	int disp = VICTORY_COUNT - 2;
-	// Tamnanho do jogo nXn
-	int m_size = BOARD_SIZE - (VICTORY_COUNT - 1);
+	int m_size = BOARD_SIZE - 2;
 
 	// Percorre o jogo a matriz dentro do jogo, isto é, eliminando as
 	// bordas da tabela, com uma matriz de mXm, onde m é o número de
 	// espaços consecutivos necessários para ganhar o jogo
-	for (i = 1, j = 1; i <= m_size; j += 1) 
+	for (i = 1, j = 1; i <= m_size; j += 1)
 	{
 		// Jogador na posição central da matriz de busca
 		mid_p = b.places[i][j];
-		has_won_d = 1;
 
-		for (k = 1; k <= disp; k += 1) 
-		{
-			// Checa as diagonais
-			if (b.places[i - k][j - k] != mid_p
-			 || b.places[i + k][j + k] != mid_p) 
-			{
-				has_won_d = 0;
-			}
-		}
+        if (b.places[i - 1][j - 1] == mid_p && b.places[i + 1][j + 1] == mid_p)
+        {
+            return mid_p;
+        }
 
-		if (has_won_d) 
-		{
-			return mid_p;
-		}
+        if (b.places[i + 1][j - 1] == mid_p && b.places[i - 1][j + 1] == mid_p)
+        {
+            return mid_p;
+        }
 
-		has_won_ad = 1;
-
-		for (k = 1; k <= disp; k += 1) 
-		{
-			// Checa as antidiagonais
-			if (b.places[i + k][j - k] != mid_p
-			 || b.places[i - k][j + k] != mid_p) 
-			{
-				has_won_ad = 0;
-			}
-		}
-
-		if (has_won_ad) 
-		{
-			return mid_p;
-		}
-
-		for (k = i - disp, m = j - disp; k <= i + disp; k += 1, m += 1) 
-		{
-			// Jogador no canto da matriz 
-			lc_p = b.places[k][m];
-
-			has_won_row = 1;
-			has_won_col = 1;
-
-			for (n = 1; n <= disp + 1; n += 1) 
-			{
-				if (lc_p != b.places[k][m + n])
-				{
-					has_won_row = 0;
-				}
-
-				if (lc_p != b.places[k + n][m]) 
-				{
-					has_won_col = 0;
-				}
-			}
-
-			if (has_won_row) 
-			{
-				return lc_p;
-			}
-
-			if (has_won_col) 
-			{
-				return lc_p;
-			}
-		}
+        row_p = b.places[i - 1][j - 1];
+        col_p = b.places[i - 1][j - 1];
 
 
-		if (j == BOARD_SIZE - 1) 
+        if (row_p == b.places[i][j - 1] && row_p == b.places[i + 1][j - 1])
+        {
+            return row_p;
+        }
+
+        if (col_p == b.places[i - 1][j] && col_p == b.places[i - 1][j + 1])
+        {
+            return col_p;
+        }
+
+		if (j == BOARD_SIZE - 1)
 		{
 			i += 1;
 			j = 1;
@@ -231,15 +185,15 @@ Player has_winner(Board b)
 	return E;
 }
 
-int is_velha(Board b) 
+int is_velha(Board b)
 {
 	int i, j;
 
-	for (i = 0; i < BOARD_SIZE; i += 1) 
+	for (i = 0; i < BOARD_SIZE; i += 1)
 	{
-		for (j = 0; j < BOARD_SIZE; j += 1) 
+		for (j = 0; j < BOARD_SIZE; j += 1)
 		{
-			if (b.places[i][j] != E) 
+			if (b.places[i][j] != E)
 			{
 				return 0;
 			}
@@ -249,7 +203,7 @@ int is_velha(Board b)
 	return 1;
 }
 
-int main(void) 
+int main(void)
 {
 	Board b = new_board();
 	Player who_is = X;
@@ -263,39 +217,39 @@ int main(void)
 	char choice;
 	int col, row;
 
-	while ( ! has_choose) 
+	while ( ! has_choose)
 	{
 		printf("Você gostaria de jogar contra a máquina? [Y/N]\n");
 		scanf(" %c", &choice);
 
-		if (comp_char(choice, 'Y') || comp_char(choice, 'y')) 
+		if (comp_char(choice, 'Y') || comp_char(choice, 'y'))
 		{
 			is_against_machine = 1;
 			has_choose = 1;
 		}
-		else if (comp_char(choice, 'N') || comp_char(choice, 'n')) 
+		else if (comp_char(choice, 'N') || comp_char(choice, 'n'))
 		{
 			has_choose = 1;
 		}
 	}
-	
-	while ( ! has_end) 
+
+	while ( ! has_end)
 	{
 		print_board(b);
 
-		if ( ! is_against_machine && ! is_machine_turn) 
+		if ( ! is_against_machine && ! is_machine_turn)
 		{
-			if (who_is == X) 
+			if (who_is == X)
 			{
 				printf("Vez do jogador X:\n");
 			}
-			else 
+			else
 			{
 				printf("Vez do jogador O:\n");
 			}
 
-			while ( ! is_valid_move) 
-			{			
+			while ( ! is_valid_move)
+			{
 				printf("linha: ");
 				scanf(" %d", &row);
 
@@ -304,7 +258,7 @@ int main(void)
 
 				is_valid_move = check_move(&b, row, col);
 
-				if ( ! is_valid_move) 
+				if ( ! is_valid_move)
 				{
 					printf("O movimento %d,%d não é válido.\n", row, col);
 				}
@@ -315,26 +269,26 @@ int main(void)
 			b.places[row][col] = who_is;
 
 
-			if (is_against_machine) 
+			if (is_against_machine)
 			{
 				is_machine_turn = 1;
 			}
-		} 
-		else 
+		}
+		else
 		{
 		}
 
 		winner = has_winner(b);
 
-		if (winner != E) 
+		if (winner != E)
 		{
 			print_board(b);
 
-			if (winner == X) 
+			if (winner == X)
 			{
 				printf("O jogador X venceu!\n");
 			}
-			else 
+			else
 			{
 				printf("O jogador O venceu!\n");
 			}
@@ -342,33 +296,34 @@ int main(void)
 			has_end = 1;
 		}
 
-		if (who_is == X) 
+		if (who_is == X)
 		{
 			who_is = O;
 		}
-		else 
+		else
 		{
 			who_is = X;
 		}
 
 		b.round += 1;
 
-		if ( ! has_end && is_velha(b)) 
+		if ( has_end || is_velha(b) )
 		{
 			has_choose = 0;
 
-			while ( ! has_choose) 
+			while ( ! has_choose)
 			{
 				printf("Deu Velha! Deseja repetir? [Y/N]\n");
 
 				scanf(" %c", &choice);
 
-				if (comp_char(choice, 'Y') || comp_char(choice, 'y')) 
+				if (comp_char(choice, 'Y') || comp_char(choice, 'y'))
 				{
 					b = new_board();
 					has_choose = 1;
+					has_end = 0;
 				}
-				else if (comp_char(choice, 'N') || comp_char(choice, 'n')) 
+				else if (comp_char(choice, 'N') || comp_char(choice, 'n'))
 				{
 					has_end = 1;
 					has_choose = 1;
